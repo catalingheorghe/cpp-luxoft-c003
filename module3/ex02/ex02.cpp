e63995b896ac7597147c1db020180948380462b4
catalingheorghe <16493407+catalingheorghe@users.noreply.github.com>
2025-02-20 12:30:12 +0200

    m3:ex02 add transactionList to super class; move enum inside

--- module3/ex02/ex02.cpp
+++ module3/ex02/ex02.cpp
@@ -1,21 +1,54 @@
 /* Account class, two derived subclasses */
 
 #include <iostream>
+#include <vector>
 
 class Account {
 protected:
     int accountNumber;
     std::string owner;
     double balance;
-public:
-    Account (int number, std::string& name) : accountNumber{number}, owner{name}, balance{0} { }
+    std::vector<std::string> transactions;
+    enum OperationType { Credit, Debit };
 
-    virtual void deposit(double amount) { balance += amount; }
-    virtual void withdraw(double amount) { balance -= amount; }
+    void logTransaction(OperationType type, double amount, double balance) {
+        std::string tran = "";
+        if (type == Credit) {
+            tran += "> Credit ";
+        } else if (type == Debit) {
+            tran += "> Debit  ";
+        } else {
+            return;
+        }
+        tran += std::to_string(amount);
+        tran += " (balance: ";
+        tran += std::to_string(balance);
+        tran += ")\n";
+        transactions.insert(transactions.begin(), tran);
+    }
+
+public:
+    Account (int number, std::string& name)
+        : accountNumber{number}, owner{name}, balance{0}, transactions{}  { }
+
+    virtual void deposit(double amount) {
+        balance += amount;
+        logTransaction(Credit, amount, balance);
+    }
+    virtual void withdraw(double amount) {
+        balance -= amount;
+        logTransaction(Debit, amount, balance);
+    }
 
     void print() const {
         std::cout << "account(" << accountNumber << " " << owner 
             << " : " << balance << ");" << std::endl;
+    }
+
+    void listTransactions() const {
+        for (std::string t : transactions) {
+            std::cout << t;
+        }
     }
 };
 
@@ -26,6 +59,7 @@ public:
     void withdraw(double amount) override {
         if (amount <= balance) {
             balance -= amount;
+            logTransaction(Debit, amount, balance);
         } else {
             std::cout << "Balance insufficient" << std::endl;
         }
@@ -40,11 +74,13 @@ public:
     void withdraw(double amount) {
         if (amount <= balance) {
             balance -= amount;
+            logTransaction(Debit, amount, balance);
         } else {
             double fee = amount * 0.01;
             balance -= amount;
             std::cout << "Going below 0 with a " << fee << "$ fee" << std::endl;
             balance -= fee;
+            logTransaction(Debit, amount + fee, balance);
         }
     }
 };
@@ -58,6 +94,7 @@ int main()
     acc.deposit(2000);
     acc.withdraw(1500);
     acc.print(); // 3500
+    acc.listTransactions();
 
     name = "Savester";
     SavingsAccount savings{2, name};
@@ -65,6 +102,7 @@ int main()
     savings.withdraw(4000); // 1000
     savings.withdraw(3000); // still 1000
     savings.print();
+    savings.listTransactions();
 
     name = "Crediter";
     CheckingAccount credit{3, name};
@@ -72,6 +110,7 @@ int main()
     credit.withdraw(4000);
     credit.withdraw(3000);
     credit.print();
+    credit.listTransactions();
 
     std::cout << std::endl;
 
